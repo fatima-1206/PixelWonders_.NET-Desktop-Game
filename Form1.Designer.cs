629867b64047345bc9e34cb9488ff402e392f70a
Ayman Irshad <aymanirshad2005@gmail.com>
2025-03-06 15:12:36 +0500

    Added Form2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            imageList1 = new ImageList(components);
+            label1 = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            button2 = new Button();
+            button3 = new Button();
+            button1 = new Button();
+            panel3 = new Panel();
+            label2 = new Label();
+            button4 = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth32Bit;
+            imageList1.ImageSize = new Size(16, 16);
+            imageList1.TransparentColor = Color.Transparent;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(372, 120);
+            label1.Name = "label1";
+            label1.Size = new Size(0, 15);
+            label1.TabIndex = 0;
+            label1.Click += Label1_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(465, 69);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(306, 321);
+            panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(255, 210, 160);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(button1);
+            panel2.Location = new Point(17, 14);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(274, 294);
+            panel2.TabIndex = 0;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(168, 136, 181);
+            button2.Font = new Font("Pixelify Sans", 12F);
+            button2.ForeColor = Color.FromArgb(64, 0, 64);
+            button2.Location = new Point(159, 107);
+            button2.Name = "button2";
+            button2.Size = new Size(99, 48);
+            button2.TabIndex = 3;
+            button2.Text = "Puzzle";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(168, 136, 181);
+            button3.Font = new Font("Pixelify Sans", 12F);
+            button3.ForeColor = Color.FromArgb(64, 0, 64);
+            button3.Location = new Point(86, 206);
+            button3.Name = "button3";
+            button3.Size = new Size(106, 48);
+            button3.TabIndex = 2;
+            button3.Text = "Color by \r\nnumber";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(168, 136, 181);
+            button1.Font = new Font("Pixelify Sans", 12F);
+            button1.ForeColor = Color.FromArgb(64, 0, 64);
+            button1.Location = new Point(19, 107);
+            button1.Name = "button1";
+            button1.Size = new Size(99, 48);
+            button1.TabIndex = 0;
+            button1.Text = "Sudoku";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += Button1_Click;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.Thistle;
+            panel3.Controls.Add(label2);
+            panel3.Font = new Font("Pixelify Sans", 12F);
+            panel3.Location = new Point(0, 161);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(805, 100);
+            panel3.TabIndex = 2;
+            panel3.Text = "Choose your mystery solving level";
+            panel3.Paint += Panel3_Paint;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Pixelify Sans", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.FromArgb(64, 0, 64);
+            label2.Location = new Point(80, 40);
+            label2.Name = "label2";
+            label2.Size = new Size(353, 23);
+            label2.TabIndex = 0;
+            label2.Text = "Choose your mystery solving level";
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Thistle;
+            button4.Font = new Font("Pixelify Sans", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button4.Location = new Point(12, 12);
+            button4.Name = "button4";
+            button4.Size = new Size(77, 37);
+            button4.TabIndex = 3;
+            button4.Text = ">Back";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(168, 136, 181);
+            ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(panel1);
+            Controls.Add(panel3);
+            Controls.Add(label1);
+            ForeColor = SystemColors.ControlText;
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel2.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ImageList imageList1;
+        private Label label1;
+        private Panel panel1;
+        private Panel panel2;
+        private Button button3;
+        private Button button1;
+        private Button button2;
+        private Panel panel3;
+        private Label label2;
+        private Button button4;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,21 @@ namespace PixelWonders
         {
             InitializeComponent();
         }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Form2 newForm = new Form2();  // Create an instance of Form2
+            newForm.Show();
+        }
     }
 }
