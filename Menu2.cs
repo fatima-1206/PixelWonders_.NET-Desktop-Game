5b5b3ee073f9add9606d11cda8e25f46c09e223e
Ayman Irshad <aymanirshad2005@gmail.com>
2025-03-07 09:18:41 +0500

    Added Menu1, Menu2, and Login

---
+++ Menu2.cs
@@ -0,0 +1,37 @@
+namespace PixelWonders
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            Form2 newForm = new Form2();  // Create an instance of Form2
+            newForm.Show();
+            this.Close();
+        }
+
+        private void Label3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
