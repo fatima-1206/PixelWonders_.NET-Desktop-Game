24f871d47a2aaa43f8308f60c30fa214b922c09d
Fatima Tu Zahra <156695063+hrafati-matuza@users.noreply.github.com>
2025-03-26 20:52:10 +0500

    Added a form for creating the grid

---
+++ createDesign.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace PixelWonders
+{
+    partial class createDesign
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel2 = new Panel();
+            pictureBox2 = new PictureBox();
+            addButton = new Panel();
+            header = new Panel();
+            label1 = new Label();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            panel5 = new Panel();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            header.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel2
+            // 
+            panel2.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            panel2.Controls.Add(pictureBox2);
+            panel2.Controls.Add(addButton);
+            panel2.Controls.Add(header);
+            panel2.Location = new Point(10, 32);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1133, 80);
+            panel2.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.logo3;
+            pictureBox2.Location = new Point(2, 6);
+            pictureBox2.Margin = new Padding(2);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(126, 72);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // addButton
+            // 
+            addButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            addButton.BackgroundImage = Properties.Resources.add1;
+            addButton.BackgroundImageLayout = ImageLayout.Center;
+            addButton.Cursor = Cursors.Hand;
+            addButton.Location = new Point(1954, 6);
+            addButton.Margin = new Padding(2);
+            addButton.Name = "addButton";
+            addButton.Padding = new Padding(15);
+            addButton.Size = new Size(109, 72);
+            addButton.TabIndex = 2;
+            addButton.Tag = "";
+            // 
+            // header
+            // 
+            header.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            header.Controls.Add(label1);
+            header.Location = new Point(132, 6);
+            header.Margin = new Padding(2);
+            header.Name = "header";
+            header.Size = new Size(1818, 69);
+            header.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoEllipsis = true;
+            label1.AutoSize = true;
+            label1.Font = new Font("Pixelify Sans", 34F, FontStyle.Bold);
+            label1.ForeColor = Color.FromArgb(168, 136, 181);
+            label1.Location = new Point(203, -5);
+            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(479, 69);
+            label1.TabIndex = 0;
+            label1.Text = "Create a Design";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.BackColor = Color.FromArgb(188, 176, 201);
+            flowLayoutPanel1.Location = new Point(53, 143);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(500, 500);
+            flowLayoutPanel1.TabIndex = 4;
+            // 
+            // panel5
+            // 
+            panel5.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            panel5.BackgroundImage = Properties.Resources.cross1;
+            panel5.BackgroundImageLayout = ImageLayout.None;
+            panel5.Cursor = Cursors.Hand;
+            panel5.Location = new Point(1091, 0);
+            panel5.Margin = new Padding(2);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(52, 34);
+            panel5.TabIndex = 5;
+            // 
+            // createDesign
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1143, 750);
+            Controls.Add(panel5);
+            Controls.Add(flowLayoutPanel1);
+            Controls.Add(panel2);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "createDesign";
+            Text = "createDesign";
+            Load += createDesign_Load;
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            header.ResumeLayout(false);
+            header.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel2;
+        private PictureBox pictureBox2;
+        private Panel addButton;
+        private Panel header;
+        private Label label1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Panel panel5;
+    }
+}
---
+++ createDesign.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PixelWonders
+{
+    public partial class createDesign : Form
+    {
+        public createDesign()
+        {
+            InitializeComponent();
+        }
+
+        private void createDesign_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
