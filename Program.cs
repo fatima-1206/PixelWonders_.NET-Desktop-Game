84ac01e25dfdc438c8e089a315bd90eac9dba52e
Fatima Tu Zahra <156695063+hrafati-matuza@users.noreply.github.com>
2025-04-28 01:29:00 +0500

    .

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace PixelWonders
             dbManager.AddPredefinedPalettesToDatabase();
 
 
-            Application.Run(new PaletteForm());
+            Application.Run(new Home());
 
         }
     }
